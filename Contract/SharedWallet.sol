64528b387e465c6a6c20e6daf1eab4b40aad89d9
Roumez <remi.roumezy@hotmail.fr>
2021-06-08 20:16:44 +0200

    go

--- Contract/SharedWallet.sol
+++ Contract/SharedWallet.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SharedWallet {
+contract SharedWallet is Ownable {
 
 // Mapping from account addresses to current balance.
 mapping (address => uint256) private _balances;
